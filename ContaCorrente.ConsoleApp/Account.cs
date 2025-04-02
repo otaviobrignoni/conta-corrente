3c25d06dd3ed68394f9b50f621ac2f7f851bd5e5
Otávio Rodrigo Pereira Brignoni <otavio.r01@aluno.ifsc.edu.br>
2025-04-02 17:56:32 -0300

    Adicionada logica da conta corrente

---
+++ ContaCorrente.ConsoleApp/Account.cs
@@ -0,0 +1,74 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    class Account
+    {
+        int accountNumber;
+        Queue<string> accountLog = new Queue<string>();
+        decimal balance;
+        decimal withdrawalLimit = -500.00m;
+
+        public Account(int accountNumber)
+        {
+            this.accountNumber = accountNumber;
+            balance = 1000.00m;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+            AddLogEntry($"Depósito: +R${amount}");
+        }
+
+        public void Withdrawal(decimal amount)
+        {
+            if (balance - amount < withdrawalLimit)
+                Console.WriteLine("Saldo insuficiente");
+            else
+            {
+                balance -= amount;
+                AddLogEntry($"Saque: +R${amount}");
+            }
+        }
+
+        public void TransferBalance(decimal amount, Account account)
+        {
+            if (balance - amount < withdrawalLimit)
+                Console.WriteLine("Saldo insuficiente");
+            else
+            {
+                balance -= amount;
+                AddLogEntry($"Transfêrencia: -R${amount} p/ Conta N°{account.accountNumber}");
+                account.Deposit(amount);
+                account.AddLogEntry($"Transfêrencia: +R${amount} da Conta N°{accountNumber}");
+            }
+        }
+
+        public void ShowCurrentBalance()
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine($"Conta N°{accountNumber}: Saldo atual: R${balance:F2}");
+            Console.WriteLine("---------");
+        }
+
+        public void ShowAccountLog()
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine($"Conta N°{accountNumber}: Histórico");
+            Console.WriteLine("---------");
+            foreach (string entry in accountLog)
+            {
+                Console.WriteLine(entry);
+            }
+            Console.WriteLine("---------");
+        }
+
+        public void AddLogEntry(string record)
+        {
+            if (accountLog.Count >= 20)
+            {
+                accountLog.Dequeue();
+            }
+            accountLog.Enqueue(record);
+        }
+    }
+}
